4189890355c381e114ea5aff5601162447fa16e1
Julia S <julia297711@yandex.ru>
2023-04-21 18:55:14 +0700

    commit

---
+++ 5-languages/index1.js
@@ -0,0 +1,15 @@
+const lang = 'ru';
+
+switch(lang) {
+    case 'ru': // lang === 'ru'
+         console.log('Привет!');
+         break;
+    case 'en' : 
+         console.log('Hello!');
+         break;
+    case 'fr' : 
+         console.log('Bonjour!');
+         break;
+    default: 
+    console.log( 'I do not know your language')
+};
