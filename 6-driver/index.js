b1a8223fa1bc323df1edf84fc2792f5c93adebfc
Julia S <julia297711@yandex.ru>
2023-04-27 18:19:04 +0700

    после ревью

--- 6-driver/index.js
+++ 6-driver/index.js
@@ -3,5 +3,5 @@ const age = 19;
 const isDrunk = true;
 
 
-const canDrive = (hasLicence && age>=18 && !isDrunk);
+const canDrive = hasLicence && age >= 18 && !isDrunk;
 console.log(`Может пользователь водить машину: ${canDrive ? 'Может' : 'Не может'}`)
