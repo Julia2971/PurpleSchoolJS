905890641a6b885a51d29f7e3cafdfbe3b6d28e3
Julia S <julia297711@yandex.ru>
2023-04-28 17:04:21 +0700

    коммит ту

--- 7-converter/index.js
+++ 7-converter/index.js
@@ -1,3 +1,14 @@
-function doConversation(money){
-    let newMoney = 
+function doConv(money, currency ){
+    const rate = { 
+        euro : 0.011,
+        usd : 0.012
+    } 
+
+    if (!rate[currency]){
+        return null
+    } else {
+        return money * rate[currency] 
+    }
 }
+
+console.log(doConv(10000, 'usd'))
