afb87b9f79335dec434845f6c4dd491525279aa2
Julia S <julia297711@yandex.ru>
2023-04-28 17:04:21 +0700

    коммит ту

---
+++ 8-crypto/index.js
@@ -0,0 +1,21 @@
+function crypto(passw){
+    const changePassw = passw.split('');  
+    const partOfPassw = changePassw.splice(0, 4).reverse();
+    const newPassw = changePassw.reverse().concat(partOfPassw).join('');
+    return newPassw;
+
+
+}
+
+function check(res, passw) {
+    const makePassw = res.split('')
+    const newPartOsPassw = makePassw.splice(-4);
+    const newPassw2 = newPartOsPassw.reverse().concat(makePassw.reverse()).join('');
+    if (newPassw2 === passw){
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+};
+check(crypto('password'), 'password')
+
